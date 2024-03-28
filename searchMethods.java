fcf27b2e798e1c4c69c0a56935fa142582fb4a0a
johannapal <89807409+johannapal@users.noreply.github.com>
2024-03-28 19:55:51 -0400

    Create searchMethods.java

---
+++ searchMethods.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class searchMethods {
+    public ArrayList<String> searchUsers(String search) {
+        File dir = new File("."); // Assuming user files are in the current directory
+        File[] files = dir.listFiles();
+        ArrayList<String> matchedUsers = new ArrayList<>();
+
+        if (files != null) {
+            for (File file : files) {
+                // Get the file name and remove the .txt extension
+                String fileName = file.getName();
+                if (fileName.endsWith(".txt")) {
+                    String username = fileName.substring(0, fileName.length() - 4);
+
+                    if (username.contains(search)) {
+                        matchedUsers.add(username);
+                    }
+                }
+            }
+        }
+
+        // Return the list of matched users directly
+        return matchedUsers;
+    } //searchUsers()
+
+
+
+} //end class
