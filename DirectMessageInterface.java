79e1490800d4f117bf20c46312a4efa865872e03
johannapal <89807409+johannapal@users.noreply.github.com>
2024-03-31 23:05:14 -0400

    Update DirectMessageInterface.java

--- DirectMessageInterface.java
+++ DirectMessageInterface.java
@@ -1,4 +1,13 @@
 import java.util.List;
+/**
+ * DirectMessageInterface.java
+ * 
+ * Interface for direct message methods. 
+ *
+ * @author Johanna Palomar, Janavi Munagavalasa, Arushi Chaudhary, Valeria Paulina Cordero Salinas, Corbett Papastathis,
+ * Lecture 1, Lab 10
+ * @version 3/25/2024
+ */
 public interface DirectMessageInterface {
     public String getFileName(User currentUser, User searchedUser);
     public boolean openMessages(User currentUser, User searchedUser);
