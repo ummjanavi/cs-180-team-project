f2176dc3f0b969971915169731221fcbd6e783e4
johannapal <89807409+johannapal@users.noreply.github.com>
2024-03-31 23:10:14 -0400

    Update GuiInterface.java

--- GuiInterface.java
+++ GuiInterface.java
@@ -1,6 +1,14 @@
 import java.io.IOException;
 import java.net.UnknownHostException;
-
+/**
+ * GuiInterface.java
+ * 
+ * This is an interface for a future GUI class.
+ *
+ * @author Johanna Palomar, Janavi Munagavalasa, Arushi Chaudhary, Valeria Paulina Cordero Salinas, Corbett Papastathis,
+ * Lecture 1, Lab 10
+ * @version 3/25/2024
+ */
 public interface GuiInterface {
     /*
     public static void main(String[] args) throws IOException, ClassNotFoundException, UnknownHostException {
