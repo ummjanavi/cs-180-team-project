2f9dcc428dd17cfed13a3226440e1e1c3a305386
ummjanavi <143826654+ummjanavi@users.noreply.github.com>
2024-03-31 20:51:11 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -11,11 +11,11 @@ public interface UserInterface {
     public void setFriends(ArrayList<String> friends);
     public ArrayList<String> getBlocked();
     public void setBlocked(ArrayList<String> blocked);
-    public void addFriend(TeamProject.User user);
-    public void removeFriend(TeamProject.User user);
-    public void blockUser(TeamProject.User user);
-    public void unblockUser(TeamProject.User user);
-    public boolean equals(TeamProject.User user);
+    //public void addFriend(TeamProject.User user);
+    //public void removeFriend(TeamProject.User user);
+    //public void blockUser(TeamProject.User user);
+    //public void unblockUser(TeamProject.User user);
+    //public boolean equals(TeamProject.User user);
     public String toString(Boolean openMessaging);
     public String toString(ArrayList<String> usernames);
     public boolean writeToFile();
