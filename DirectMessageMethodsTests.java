8602451b241c66941dafa139b09bc080200c9e9e
johannapal <89807409+johannapal@users.noreply.github.com>
2024-03-31 22:05:12 -0400

    Update DirectMessageMethodsTests.java

--- DirectMessageMethodsTests.java
+++ DirectMessageMethodsTests.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
 
-public class DirectMessageMethodsTest {
+public class DirectMessageMethodsTests {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(TestCase.class);
         System.out.printf("Test Count: %d.\n", result.getRunCount());
