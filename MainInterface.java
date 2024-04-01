d1e54559af206595d7c3df263881b95151a5cee1
corpap <157079530+corpap@users.noreply.github.com>
2024-03-31 21:33:13 -0400

    Create MainInterface.java

---
+++ MainInterface.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public interface MainInterface {
+    private static void showLoginMenu() {
+    }
+    private static void loginProcess(Scanner scan) {
+    }
+    private static void accountCreationProcess(Scanner scan) {
+    }
+    private static void showMainMenu(User currentUser, Scanner scan) {
+    }
+    public static void showAccountSettings(User currentUser, Scanner scan) {
+    }
+    public static void changePasswordProcess(User currentUser, Scanner scan) {
+    }
+    public static void changeDirectMessageSetting(User currentUser, Scanner scan) {
+    }
+    public static void searchProcess(User currentUser, Scanner scan) {
+    }
+    public static void userViewerMenu(User currentUser, User searchedUser, Scanner scan) {
+    }
+    public static void directMessageMenu(User currentUser, User searchedUser, Scanner scan) {
+    }
+}
