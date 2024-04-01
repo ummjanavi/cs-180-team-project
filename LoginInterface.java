9c7cb88bb0dea63d12fec6108a8b08cd190e234d
johannapal <89807409+johannapal@users.noreply.github.com>
2024-03-31 23:10:55 -0400

    Update LoginInterface.java

--- LoginInterface.java
+++ LoginInterface.java
@@ -1,3 +1,12 @@
+/**
+ * LoginInterface.java
+ * 
+ * Interface for login methods.
+ *
+ * @author Johanna Palomar, Janavi Munagavalasa, Arushi Chaudhary, Valeria Paulina Cordero Salinas, Corbett Papastathis,
+ * Lecture 1, Lab 10
+ * @version 3/25/2024
+ */
 public interface LoginInterface {
     public boolean checkUsername(String username);
     public boolean createAccount(String username, String password);
