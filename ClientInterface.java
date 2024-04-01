04fff47611009ac576239b5d0318ca5aca1dadd0
corpap <157079530+corpap@users.noreply.github.com>
2024-03-31 22:02:48 -0400

    Create ClientInterface.java

---
+++ ClientInterface.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public interface ClientInterface {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, UnknownHostException {
+    }
+}
