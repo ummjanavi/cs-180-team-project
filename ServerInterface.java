7d17adaff1ce8b05ed0727449986d59142c6e6ff
johannapal <89807409+johannapal@users.noreply.github.com>
2024-03-31 23:25:02 -0400

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,4 +1,13 @@
 import java.io.IOException;
+/**
+ * ServerInterface.java
+ * 
+ * This is the interface for a future server class.
+ *
+ * @author Johanna Palomar, Janavi Munagavalasa, Arushi Chaudhary, Valeria Paulina Cordero Salinas, Corbett Papastathis,
+ * Lecture 1, Lab 10
+ * @version 3/25/2024
+ */
 
 public interface ServerInterface {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
