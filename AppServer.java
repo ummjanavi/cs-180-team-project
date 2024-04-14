499dcf371ff09f3ee6159b23b2f64a9c642a5e3b
achaudhary17 <164261782+achaudhary17@users.noreply.github.com>
2024-04-14 13:51:26 -0400

    Update AppServer.java

--- AppServer.java
+++ AppServer.java
@@ -1,14 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class AppServer implements ServerInterface {
 
     private static final int Default_Port = 4545;
+    private static LoginMethods loginMethods = new LoginMethods();
+    private static SearchMethods searchMethods = new SearchMethods();
+    private static DirectMessageMethods directMessageMethods = new DirectMessageMethods();
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         try {
             ServerSocket serverSocket = new ServerSocket(Default_Port);
@@ -19,10 +21,20 @@ public class AppServer implements ServerInterface {
                     Socket socket = serverSocket.accept();
                     System.out.println("Client connected: " + socket.getInetAddress());
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                   PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                    //ObjectOutputStream writer = new ObjectOutputStream(socket.getOutputStream());
+
                     String searchQuery = reader.readLine();
                     System.out.println("From Client -> " + searchQuery);
-                    writer.println("Data");
+                    //writer.writeObject(" From Server Hello");
+                    /*
+                      send data to server like
+                      user:username|password|createaccount
+                    */
+                    String result = parseData(searchQuery,writer);
+                    if(!Objects.equals(result, ""))
+                        writer.println(result);
+                        //writer.writeObject(result);
                     writer.flush();
                     socket.close();
                 }
@@ -32,6 +44,34 @@ public class AppServer implements ServerInterface {
         }
         catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String parseData(String clientInput, PrintWriter writer) throws IOException {
+        // check for user
+        if(clientInput.contains("user:")) {
+            clientInput = clientInput.replace("user:","");
+            String username, password;
+            username = clientInput.split("|")[0];
+            password = clientInput.split("|")[1];
+            accountCreation(username, password);
+        }
+        // search for user
+        else if (clientInput.contains("searchuser:")){
+            String currentUser = clientInput.replace("searchuser:","");
+            ArrayList<String> fullResult = searchMethods.searchUsers(currentUser);
+            for(String result: fullResult) {
+                writer.println(result);
+            }
+        }
+        return "";
+    }
+    private static void accountCreation(String username, String password) {
+        if (loginMethods.createAccount(username, password)) { //if the account is created successfully
+            System.out.println("Account Created Successfully. Sign in using 'Login'");
+            new User(username, password);
+        } else {
+            System.out.println("There was an error creating your account. Please try again.");
         }
     }
 }
