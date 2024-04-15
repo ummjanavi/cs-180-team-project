19848b9b5ed8d60522964765fd4e9ace4fc5c0b0
ummjanavi <143826654+ummjanavi@users.noreply.github.com>
2024-04-14 22:54:06 -0400

    Update AppServer.java

--- AppServer.java
+++ AppServer.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class AppServer implements ServerInterface {
@@ -18,16 +19,14 @@ public class AppServer implements ServerInterface {
             ServerSocket serverSocket = new ServerSocket(Default_Port);
             System.out.println("Waiting for the client to connect...");
 
-            MainApplication m = new MainApplication(new Socket("localhost",4545));
-            m.start();
-
             try {
                 while(true) {
                     Socket socket = serverSocket.accept();
                     System.out.println("Client connected: " + socket.getInetAddress());
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-                    while (true) {
+                    boolean exit = false;
+                    do {
                         String username = "";
                         String input = reader.readLine();
                         if (input.equals("CHECK_USERNAME")) {
@@ -55,10 +54,52 @@ public class AppServer implements ServerInterface {
                             currentUser.setOpenMessaging(true);
                         } else if (input.equals("SET_OPEN_MESSAGING")) {
                             username = reader.readLine();
+                            boolean oldSetting = Boolean.parseBoolean(reader.readLine());
                             User currentUser = new User(username);
-                            currentUser.setOpenMessaging(true);
+                            currentUser.setOpenMessaging(oldSetting);
+                        } else if (input.equals("SET_OPEN_MESSAGING_FALSE")) {
+                            username = reader.readLine();
+                            User currentUser = new User(username);
+                            currentUser.setOpenMessaging(false);
+                        } else if (input.equals("DISPLAY_PROFILE")) {
+                            String selectedUser = reader.readLine();
+                            User searchedUser = new User(selectedUser);
+                            searchedUser.displayProfile();
+                        } else if (input.equals("OPEN_MESSAGES")) {
+                            User currentUser = new User(reader.readLine());
+                            User searchedUser = new User(reader.readLine());
+                            boolean output = directMessageMethods.openMessages(currentUser, searchedUser);
+                            writer.write(String.valueOf(output));
+                            writer.println();
+                            writer.flush();
+                        } else if (input.equals("READ_MESSAGES")) {
+                            User currentUser = new User(reader.readLine());
+                            User searchedUser = new User(reader.readLine());
+                            List<String> messages = directMessageMethods.readMessages(currentUser, searchedUser);
+                            boolean output = directMessageMethods.displayMessages(messages);
+                            writer.write(String.valueOf(output));
+                            writer.println();
+                            writer.flush();
+                        } else if (input.equals("SEND_MESSAGE")) {
+                            User currentUser = new User(reader.readLine());
+                            User searchedUser = new User(reader.readLine());
+                            String message = reader.readLine();
+                            boolean output = directMessageMethods.sendMessage(currentUser, searchedUser, message);
+                            writer.write(String.valueOf(output));
+                            writer.println();
+                            writer.flush();
+                        } else if (input.equals("WRITE_MESSAGES")) {
+                            User currentUser = new User(reader.readLine());
+                            User searchedUser = new User(reader.readLine());
+                            List<String> messages = directMessageMethods.readMessages(currentUser, searchedUser);
+                            boolean output = directMessageMethods.writeMessages(currentUser, searchedUser, messages);
+                            writer.write(String.valueOf(output));
+                            writer.println();
+                            writer.flush();
+                        } else if (input.equals("exit")) {
+                            exit = true;
                         }
-                    }
+                    } while (!exit);
 
                     socket.close();
                 }
