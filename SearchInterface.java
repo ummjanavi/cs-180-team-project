18d10b6645fa44353eb302eb60010732462d4998
corpap <157079530+corpap@users.noreply.github.com>
2024-04-15 14:19:12 -0400

    Update SearchInterface.java

--- SearchInterface.java
+++ SearchInterface.java
@@ -1,6 +1,13 @@
-package TeamProject;
-
 import java.util.ArrayList;
+/**
+ * SearchInterface.java
+ * 
+ * This is the interface for SearchMethods.
+ *
+ * @author Johanna Palomar, Janavi Munagavalasa, Arushi Chaudhary, Valeria Paulina Cordero Salinas, Corbett Papastathis,
+ * Lecture 1, Lab 10
+ * @version 3/25/2024
+ */
 public interface SearchInterface {
     public ArrayList<String> searchUsers(String search);
 }
