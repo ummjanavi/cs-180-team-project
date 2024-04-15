1608a59eec2815f5362502425a1719b6f9d8aadc
corpap <157079530+corpap@users.noreply.github.com>
2024-04-15 14:17:22 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,6 +1,13 @@
-package TeamProject;
-
 import java.util.ArrayList;
+/**
+ * UserInterface.java
+ * 
+ * This is the interface for the user class.
+ *
+ * @author Johanna Palomar, Janavi Munagavalasa, Arushi Chaudhary, Valeria Paulina Cordero Salinas, Corbett Papastathis,
+ * Lecture 1, Lab 10
+ * @version 3/25/2024
+ */
 public interface UserInterface {
     public String getUsername();
     public String getPassword();
